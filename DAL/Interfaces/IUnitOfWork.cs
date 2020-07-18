2ad605d154f64b13717ab3209e1e39bae6f09486
Murk <32967078+nydopustimname@users.noreply.github.com>
2020-07-18 03:47:51 +0300

    dal

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using EstChe.Models;
+using System;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Item> Itemss { get; }
+        IRepository<Order> Orders { get; }
+        void Save();
+
+        
+    }
+}
---
+++ DAL/Interfaces/IUnitOfWorkIdentity.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Identity;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+   public interface IUnitOfWorkIdentity
+    {
+        AppUserManager UserManager { get; }
+        IClientManager ClientManager { get; }
+        AppRoleManager RoleManager { get; }
+        Task SaveAsync();
+    }
+}
