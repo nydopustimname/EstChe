bfc9bc48346e019e6cdaee37e4f96c37e1a20627
Murk <32967078+nydopustimname@users.noreply.github.com>
2020-07-21 04:05:17 +0300

    architecture fix, common layer

---
+++ DAL/Identity/AppRoleManager.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DAL.Identity
+{
+    public class AppRoleManager : RoleManager<AppRole>
+    {
+        public AppRoleManager(RoleStore<AppRole> store)
+                    : base(store)
+        { }
+    }
+    
+}
---
+++ DAL/Interfaces/IAppRoleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IAppRoleManager : IUnitOfWork
+    {
+        Task CreateAsync(object role);
+        Task FindByNameAsync(string roleName);
+    }
+}
